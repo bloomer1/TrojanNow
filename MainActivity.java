c33b01bc0c24c9438f8504d3e1efc9291bd9be89
Rahul Agarwal <reams.al@gmail.com>
2015-04-14 17:11:28 -0700

    Intial login UI

--- MainActivity.java
+++ MainActivity.java
@@ -1,47 +1,36 @@
-package com.example.rahulagarwal.environment;
+package com.example.rahulagarwal.trojannowfl;
 
 import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements SensorEventListener{
+public class MainActivity extends Activity {
 
-    private SensorManager mSensorManager;
-    private Sensor mTemperature;
-    private TextView text;
-    private StringBuilder msg = new StringBuilder(2048);
-
-
-
+    private EditText  username=null;
+    private EditText password=null;
+    private TextView attempts;
+    private Button login;
+    int counter = 3;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Get an instance of the sensor service, and use that to get an instance of
-        // a particular sensor.
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mTemperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
-        text = (TextView) findViewById(R.id.text);
-
-
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+        //attempts = (TextView)findViewById(R.id.textView5);
+        //attempts.setText(Integer.toString(counter));
+        login = (Button)findViewById(R.id.button1);
     }
-
-
 
 
     @Override
@@ -49,6 +38,18 @@ public class MainActivity extends Activity implements SensorEventListener{
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void login(View view){
+        Login_Util lu = new Login_Util("Login_Util");
+        lu.login(view,getApplicationContext(),username,password,login);
+
+
+
+        }
+
+    public void signup(){
+
     }
 
     @Override
@@ -64,80 +65,5 @@ public class MainActivity extends Activity implements SensorEventListener{
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // The activity is about to become visible.
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // The activity has become visible (it is now "resumed").
-        //Register the listener for the Sensor Service
-        mSensorManager.registerListener(this, mTemperature, SensorManager.SENSOR_DELAY_NORMAL);
-
-
-
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Another activity is taking focus (this activity is about to be "paused").
-        // Be sure to unregister the sensor when the activity pauses.
-
-        mSensorManager.unregisterListener(this);
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // The activity is no longer visible (it is now "stopped")
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // The activity is about to be destroyed.
-
-
-
-
-
-    }
-
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
-        text.setText(String.valueOf(msg));
-        text.invalidate();
-        Log.i("Rahul",msg.toString());
-        Context context = getApplicationContext();
-
-        CharSequence text = "Hello toast!";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-
-        float fahrenheit = sensorEvent.values[0] * 9 / 5 + 32;
-        Log.i("Rahul", String.valueOf(fahrenheit));
-        msg.insert(0, "Got a sensor event: " + sensorEvent.values[0] + " Celsius (" +
-                fahrenheit  + " F)\n");
-        Log.i("Rahul11", "Activity created");
-        text.setText(String.valueOf(msg));
-        text.invalidate();
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
     }
 }
