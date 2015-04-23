1e69986d926c28efe6e42837342e45f3b9196215
bloomer1 <rahulaga@usc.edu>
2015-04-23 00:10:59 -0700

    Update WeatherHttpClient.java

--- new/WeatherHttpClient.java
+++ new/WeatherHttpClient.java
@@ -10,25 +10,11 @@ package com.example.rahulagarwal.trojannowfl2;
         import java.io.InputStreamReader;
         import java.net.HttpURLConnection;
         import java.net.URL;
-/*
- * Copyright (C) 2013 Surviving with Android (http://www.survivingwithandroid.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 public class WeatherHttpClient {
 
     private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?lat=";
-   // private static String IMG_URL = "http://openweathermap.org/img/w/";
+   
 
 
     public String getWeatherData(String lat, String lon) {
@@ -67,35 +53,5 @@ public class WeatherHttpClient {
 
     }
 
-//    public byte[] getImage(String code) {
-//        HttpURLConnection con = null ;
-//        InputStream is = null;
-//        try {
-//            con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
-//            con.setRequestMethod("GET");
-//            con.setDoInput(true);
-//            con.setDoOutput(true);
-//            con.connect();
-//
-//            // Let's read the response
-//            is = con.getInputStream();
-//            byte[] buffer = new byte[1024];
-//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//
-//            while ( is.read(buffer) != -1)
-//                baos.write(buffer);
-//
-//            return baos.toByteArray();
-//        }
-//        catch(Throwable t) {
-//            t.printStackTrace();
-//        }
-//        finally {
-//            try { is.close(); } catch(Throwable t) {}
-//            try { con.disconnect(); } catch(Throwable t) {}
-//        }
-//
-//        return null;
-//
-//    }
+
 }
